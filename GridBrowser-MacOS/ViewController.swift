51030374d6aafc6db5f8daa50b0fcf4451904f6c
sadikcoban <coban.sadik@icloud.com>
2023-02-20 23:19:58 +0300

    create ui of toolbar on the main window

--- GridBrowser-MacOS/ViewController.swift
+++ GridBrowser-MacOS/ViewController.swift
@@ -21,6 +21,8 @@ class ViewController: NSViewController {
         }
     }
 
+    
+    
 
 }
 
---
+++ GridBrowser-MacOS/WindowController.swift
@@ -0,0 +1,19 @@
+//
+//  WindowController.swift
+//  GridBrowser-MacOS
+//
+//  Created by Sadık Çoban on 20.02.2023.
+//
+
+import Cocoa
+
+class WindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+        window?.titleVisibility = .hidden
+
+    }
+    
+
+}
